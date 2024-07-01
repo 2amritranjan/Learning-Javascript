cd7d191ec7b0cabc494d334fffa91b8cbd213337
2amritranjan <2amritranjan@gmail.com>
2024-07-01 05:39:07 +0000

    container config done

---
+++ 01/variable.js
@@ -0,0 +1,26 @@
+const Employee_Id = 537792
+let Employee_Email = "employee@google.com"
+var Employee_Password= "123@Abcd"
+Employee_Name = "Amrit Ranjan"
+//Employee_Id = 537793   (it is not allowed)
+//console.log(Employee_Id);
+Employee_Email ="emp@google.com"
+
+
+
+Employee_Password = "Abcd@123"
+
+
+
+Employee_Name = "Ajit Kumar"
+
+
+
+console.table([Employee_Id, Employee_Email, Employee_Password, Employee_Name])
+
+
+/*
+please don't use Var
+because of issue in block scope and functional scope
+which we will read later.
+*/
