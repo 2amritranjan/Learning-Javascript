2f259c34d8942d3ec8b1ca8eedf762080ad06bf6
2amritranjan <2amritranjan@gmail.com>
2024-07-01 07:46:13 +0000

    container config done

---
+++ 01/dataTypes.js
@@ -0,0 +1,23 @@
+let age = 25
+let weight = 62.456
+
+console.log(typeof age)
+console.log(typeof weight)
+
+console.table([age, weight])
+/*
+Number:- It represents both int and float.
+*/
+
+var firstName = "Amrit"
+let lastName = "Ranjan"
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+
+console.table([firstName, lastName])
+/*
+It represent a sequence of characters,
+string can be enclosed in double quote ("____"),
+single quote ('_____')
+*/
