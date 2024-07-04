4afdda3a86c23386d532e9876f898bdf52880bd0
2amritranjan <2amritranjan@gmail.com>
2024-07-04 06:01:19 +0000

    container config done

---
+++ Javascript/conversionOperations.js
@@ -0,0 +1,23 @@
+console.log(6 + 2)
+console.log(6 - 2)
+console.log(6 * 2)
+console.log(6 ** 2)
+console.log(6 / 2)
+
+console.log(61 % 2)
+
+// Here operations are smooth and easy because data types are same.
+// Problem starts when we start playing with string, boolean, null and undefined.
+
+console.log("3" + 2)
+console.log ("3" + 2+ 2)
+console.log(3 + 4 + "6")
+
+// Here we can see a good example of precedence just like Python.
+
+console.log (null + 5)
+console.log (undefined+ 5)
+console.log( true + 5)
+console.log(false + 5)
+
+// This is example how Js smartly convert different data types on its own.
