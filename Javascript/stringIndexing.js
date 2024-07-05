0af458c6d9634cd78be2a4871b4878715b903ad5
2amritranjan <2amritranjan@gmail.com>
2024-07-05 16:00:02 +0000

    container config done

--- Javascript/stringIndexing.js
+++ Javascript/stringIndexing.js
@@ -3,3 +3,5 @@ let newName = name
 console.log(name.length)
 console.log(name[2])
 console.log(name.indexOf('a'))
+console.log(`Hi my name is ${name}`)
+console.log(`Hey ${name}, ask anyone if they have extra pen?`)
