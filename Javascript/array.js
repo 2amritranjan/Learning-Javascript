1a6b2d36f73f00f6a0d7a632630fdf952bbce7df
2amritranjan <2amritranjan@gmail.com>
2024-07-08 07:55:05 +0000

    container config done

---
+++ Javascript/array.js
@@ -0,0 +1,13 @@
+const array = ["Amrit Ranjan", 24, "amrit@google.com", true]
+console.log(array)
+console.log(array[3])
+console.log(typeof array)
+array.push("Graduates")
+console.log(array)
+array.pop()
+console.log(array)
+array.shift()
+console.log(array)
+array.unshift("Amrit Ranjan")
+console.log(array)
+
