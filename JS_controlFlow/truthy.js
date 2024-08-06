ede34569308b61570b2fa17f44b626ddde4e5d38
Amrit Ranjan <2amritranjan@gmail.com>
2024-08-06 04:44:11 +0000

    done

---
+++ JS_controlFlow/truthy.js
@@ -0,0 +1,18 @@
+const userEmail = " amrit@google.com"
+
+if (userEmail){
+    console.log("Got user email")
+}else{
+    console.log("Dont have user email")
+}
+
+
+/*
+//falsy values
+false, 0, -0, BigInt, " ", null, undefined, NaN
+(They are falsy values)
+
+//truthy values
+"0", "false", "anyString", [], {}, function(){}
+(They are truthy values)
+*/
