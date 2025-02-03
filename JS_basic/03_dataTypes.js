f34de0eacfab1481dc8a3da31cd4a097202f62f9
Amrit Ranjan <2amritranjan@gmail.com>
2025-02-03 14:42:34 +0530

    Update 03_dataTypes.js
    more detail on datatypes.

--- JS_basic/03_dataTypes.js
+++ JS_basic/03_dataTypes.js
@@ -22,3 +22,22 @@ string can be enclosed in double quote ("____"),
 single quote ('_____')
 */
 
+
+/*
+Primitve data types are of 7 types.
+i) string
+ii) number
+iii) boolean
+iv) null
+v) undefined
+vi) symbol
+vii) BigInt
+*/
+
+/*
+Non-primitve datatype are of three types.
+i) Array
+ii) Object
+iii) Function
+*/
+
